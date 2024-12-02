cc180eabb450e62ed8dc0a79569f7dc2a7248b88
Erika <erika@cudd.io>
2024-12-02 15:35:21 -0500

    Fix db not pushing

--- apps/client/src/lib/server/db/index.ts
+++ apps/client/src/lib/server/db/index.ts
@@ -2,7 +2,7 @@ import { createDBClient } from '@helper/db';
 import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-if (!env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
+// if (!env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
 
 export const db = createDBClient({
 	url: env.DATABASE_URL,
--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,18 +1,29 @@
+import { config } from 'dotenv';
+
 import { defineConfig } from 'drizzle-kit';
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
+// export default defineConfig({
+// 	schema: './src/schema/index.ts',
+
+// 	dbCredentials: {
+// 		url: process.env.DATABASE_URL,
+// 		authToken: process.env.DATABASE_AUTH_TOKEN,
+// 	},
+
+// 	verbose: true,
+// 	strict: true,
+// 	dialect: 'turso',
+// });
+
+config({ path: '.env' });
+
 export default defineConfig({
 	schema: './src/schema/index.ts',
-
-	dbCredentials: {
-		url: process.env.DATABASE_URL,
-		authToken: process.env.DATABASE_AUTH_TOKEN,
-		// url: 'database.db',
-	},
-
-	verbose: true,
-	strict: true,
-	// driver: 'turso',
-	// dialect: 'sqlite',
+	out: './migrations',
 	dialect: 'turso',
+	dbCredentials: {
+		url: process.env.DATABASE_URL!,
+		authToken: process.env.DATABASE_AUTH_TOKEN!,
+	},
 });
