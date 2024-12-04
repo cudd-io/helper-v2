c1c2a7b27891fc5f9d87261f058279489b64414a
Erika <erika@cudd.io>
2024-12-04 13:17:40 -0500

    setup deployment config for bot server

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,22 +1,5 @@
-import { config } from 'dotenv';
-
 import { defineConfig } from 'drizzle-kit';
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-// export default defineConfig({
-// 	schema: './src/schema/index.ts',
-
-// 	dbCredentials: {
-// 		url: process.env.DATABASE_URL,
-// 		authToken: process.env.DATABASE_AUTH_TOKEN,
-// 	},
-
-// 	verbose: true,
-// 	strict: true,
-// 	dialect: 'turso',
-// });
-
-config({ path: '.env' });
 
 export default defineConfig({
 	schema: './src/schema/index.ts',
