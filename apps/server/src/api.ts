6236ae7164b7b26207bb5b0c1ffaecf4c5f99b9f
Erika <erika@cudd.io>
2024-12-04 11:37:44 -0500

    Add invite endpoint

---
+++ apps/server/src/api.ts
@@ -0,0 +1,46 @@
+import { Hono } from 'hono';
+import { commands } from './lib/features/bot/commands';
+import { DiscordBot, initializeBot } from './lib/features/bot/bot';
+import { bot } from '$lib/features/bot/state/bot-state';
+import { Client, GatewayIntentBits, OAuth2Scopes } from 'discord.js';
+
+const app = new Hono();
+
+app.get('/', (c) =>
+	c.json({
+		healthy: true,
+	}),
+);
+
+app.get('/commands/global', (c) => {
+	const commandsJSON = commands.map((cmd) => cmd.command.toJSON());
+	return c.json(commandsJSON);
+});
+
+app.get('/invite', async (c) => {
+	const client = bot.client;
+	const { redirect } = c.req.query();
+
+	const shouldRedirect = !!redirect;
+
+	const invite = client.generateInvite({
+		scopes: [
+			OAuth2Scopes.Bot,
+			OAuth2Scopes.ApplicationsCommands,
+			OAuth2Scopes.ApplicationCommandsPermissionsUpdate,
+			OAuth2Scopes.MessagesRead,
+			OAuth2Scopes.GuildsMembersRead,
+		],
+		permissions: ['Administrator'],
+	});
+
+	if (shouldRedirect) {
+		return c.redirect(invite);
+	}
+
+	return c.json({
+		invite,
+	});
+});
+
+export default app;
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,29 +1,5 @@
 import './lib/features/bot/state/bot-state';
-import { Hono } from 'hono';
-import { DiscordBot, initializeBot } from './lib/features/bot/bot';
-import { commands } from './lib/features/bot/commands';
-
-const app = new Hono();
-
-app.get('/', (c) =>
-	c.json({
-		healthy: true,
-	}),
-);
-
-app.get('/commands/global', (c) => {
-	const commandsJSON = commands.map((cmd) => cmd.command.toJSON());
-	return c.json(commandsJSON);
-});
-
-// app.get('/commands/cleanup', async (c) => {
-// 	try {
-// 		await bot.cleanup();
-// 		return c.json({ success: true });
-// 	} catch (e) {
-// 		return c.json({ success: false });
-// 	}
-// });
+import app from './api';
 
 // Handle graceful shutdown
 process.on('SIGINT', async () => {
