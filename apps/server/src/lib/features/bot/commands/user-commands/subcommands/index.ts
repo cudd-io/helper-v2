85d7d972ae3cc98572b9508cd66b1a18bfac4df7
Erika <erika@cudd.io>
2024-12-09 14:12:53 -0500

    Add manual refresh command

--- apps/server/src/lib/features/bot/commands/user-commands/subcommands/index.ts
+++ apps/server/src/lib/features/bot/commands/user-commands/subcommands/index.ts
@@ -1,5 +1,11 @@
 import { subcommandList } from './list';
 import { subcommandCreate } from './create';
 import { subcommandDelete } from './delete';
+import { subcommandRefresh } from './refresh';
 
-export default [subcommandList, subcommandCreate, subcommandDelete];
+export default [
+	subcommandList,
+	subcommandCreate,
+	subcommandDelete,
+	subcommandRefresh,
+];
---
+++ apps/server/src/lib/features/bot/commands/user-commands/subcommands/refresh.ts
@@ -0,0 +1,39 @@
+import db from '$lib/db';
+import { SimpleCommand } from '@helper/db';
+import { createSubcommand } from '$lib/features/bot/utils';
+import { bot } from '$lib/features/bot/state/bot-state';
+
+const simpleCmds = new SimpleCommand(db);
+
+export const subcommandRefresh = createSubcommand({
+	name: 'refresh',
+	command: (subcommand) =>
+		subcommand.setDescription('Refresh custom commands manually'),
+
+	do: async (interaction) => {
+		const guild = interaction.guild;
+
+		if (!guild) {
+			interaction.reply(
+				'This command can only be used within a discord server',
+			);
+			return;
+		}
+
+		try {
+			await bot.refreshSimpleCommands(guild.id);
+
+			interaction.reply({
+				content: `Successfully refreshed commands`,
+				ephemeral: true,
+			});
+		} catch (e) {
+			const adminId = '866367674980630588';
+
+			interaction.reply({
+				content: `Failed to refresh commands. \nPlease notify <@${adminId}> that there's an issue.`,
+				ephemeral: true,
+			});
+		}
+	},
+});
